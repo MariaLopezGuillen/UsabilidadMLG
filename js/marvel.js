bcb62a1a9b0e6dcf2409678539d47555c1aa3f4d
Maria <mariialg21@alumnos.ilernasevilla.com>
2024-02-24 17:00:29 +0100

    falta modales

--- js/marvel.js
+++ js/marvel.js
@@ -3,6 +3,11 @@
 //   modal.classList.add("showModal");
 // }
 
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
 
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
 
 
