4a76e3b6f4b92851915e0644be07b8f439da7fb4
Maria Minque López Guillen <122986001+MariaLopezGuillen@users.noreply.github.com>
2024-03-01 23:50:42 +0100

    modal

--- js/modal.js
+++ js/modal.js
@@ -1,28 +1,27 @@
-function openModal(elementModal, elementClose){
-	const windowModal = document.getElementById(elementModal);
-	const buttonClose = document.getElementById(elementClose);
-	const body = document.getElementsByTagName("body")[0];
+function openModal(elementModal, elementClose) {
+    const windowModal = document.getElementById(elementModal);
+    const buttonClose = document.getElementById(elementClose);
+    const body = document.getElementsByTagName("body")[0];
    
-	// Agrega la clase para el fondo negro al <body>
-	body.classList.add('body-modal-open');
+    // Agrega la clase para el fondo negro al <body>
+    body.classList.add('body-modal-open');
    
-	windowModal.style.display = "flex";
-	body.style.overflow = "hidden";
+    windowModal.style.display = "flex";
+    body.style.overflow = "hidden";
    
-	buttonClose.addEventListener("click", function(){
-	   windowModal.style.display = "none";
-	   body.style.overflow = "scroll";
-	   // Elimina la clase para el fondo negro al <body>
-	   body.classList.remove('body-modal-open');
-	});
+    buttonClose.addEventListener("click", function() {
+       windowModal.style.display = "none";
+       body.style.overflow = "scroll";
+       // Elimina la clase para el fondo negro al <body>
+       body.classList.remove('body-modal-open');
+    });
    
-	window.addEventListener("click", function(event){
-	   if (event.target == windowModal) {
-		 windowModal.style.display = "none";
-		 body.style.overflow = "scroll";
-		 // Elimina la clase para el fondo negro al <body>
-		 body.classList.remove('body-modal-open');
-	   }
-	});
-   }
-   
+    window.addEventListener("click", function(event) {
+       if (event.target == windowModal) {
+         windowModal.style.display = "none";
+         body.style.overflow = "scroll";
+         // Elimina la clase para el fondo negro al <body>
+         body.classList.remove('body-modal-open');
+       }
+    });
+}
